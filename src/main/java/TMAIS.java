7d51f9ad35a1ea6b34d4ab05de0c40171b3fcaa8
A42901 <a42901@alunos.isel.pt>
2019-06-15 17:11:32 +0100

    Added Timer

--- src/main/java/TMAIS.java
+++ src/main/java/TMAIS.java
@@ -1,32 +1,41 @@
+import org.json.JSONArray;
 import org.json.JSONObject;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Timer;
+import java.util.TimerTask;
 
-public class TMAIS {
+class Schedule extends TimerTask{
+    @Override
+    public void run() {
+        System.out.println("\nTask Started");
+        try {
+            execute();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     private static String cmioIP;
     private static String cmspIP;
 
-    public static void main(String[] args) throws IOException{
-
+    private void execute() throws IOException {
         IESDConfig cfg = new IESDConfig();
         cmioIP   = cfg.getProperty("cmioIP");
         cmspIP   = cfg.getProperty("cmspIP");
 
-
-
         if(cmioIP == null || cmspIP == null){
             cmioIP = "40.115.103.115:9090";
-            cmspIP = "13.80.133.138:9090";
+            cmspIP = "13.80.133.138:9090/api/v1/user";
         }
 
-        System.out.println(cmioIP);
+        System.out.println("CMIO IP: " + cmioIP);
         CMOIS();
 
-        System.out.println("\n"+cmspIP);
+        System.out.println("\nCMSP IP: "+cmspIP);
         CMPIS();
     }
 
@@ -44,7 +53,7 @@ public class TMAIS {
         con.setReadTimeout(5000);
         //Connection Status
         int status = con.getResponseCode();
-        System.out.println("CMPIS Response Code: "+ status);
+        System.out.println("CMPIS Response Code: " + status + "\n");
         //Read content
         BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
         String inputLine;
@@ -53,31 +62,37 @@ public class TMAIS {
             content.append(inputLine);
         }
         in.close();
-        //Create JSONObject
-        JSONObject cmpisData = new JSONObject(content.toString());
-        //Create Object
-        CMPISObject cmpisObject = new CMPISObject();
-        //CMIO data
-        cmpisObject.setCmioIBAN((cmpisData.getString("cmioIBAN")));
-        cmpisObject.setCmioNIF((cmpisData.getString("cmioNIF")));
-        cmpisObject.setCmioValue((cmpisData.getFloat("cmioValue")));
-        //User data
-        cmpisObject.setUserIBAN(cmpisData.getString("userIBAN"));
-        cmpisObject.setUserNIF(cmpisData.getString("userNIF"));
-        cmpisObject.setUserAuthorized(cmpisData.getBoolean("userAuthorized"));
-        cmpisObject.setUserValue(cmpisData.getFloat("userValue"));
-        //Print Data
-        System.out.println("CMIO IBAN: " + cmpisObject.getCmioIBAN());
-        System.out.println("CMIO NIF: " + cmpisObject.getCmioNIF());
-        System.out.println("CMIO Value: " + cmpisObject.getCmioValue());
-
-        System.out.println("User IBAN: " + cmpisObject.getUserIBAN());
-        System.out.println("User NIF: " + cmpisObject.getUserNIF());
-        System.out.println("User Authorized: " + cmpisObject.getUserAuthorized());
-        System.out.println("User Value: " + cmpisObject.getUserValue());
+        //Create JSONArray
+        JSONArray jsonArray = new JSONArray(content.toString());
+        //Create array of objects
+        CMPISObject[] cmpisObject = new CMPISObject[jsonArray.length()];
+        //Create each object
+        for (int i = 0; i < jsonArray.length(); i++) {
+            //Create JSONObject
+            JSONObject cmpisData = jsonArray.getJSONObject(i);
+            //Create Object
+            cmpisObject[i] = new CMPISObject();
+            //CMIO Data
+            //cmpisObject[i].setCmioIBAN((cmpisData.getString("cmioIBAN")));
+            //cmpisObject[i].setCmioNIF((cmpisData.getString("cmioNIF")));
+            //cmpisObject[i].setCmioValue((cmpisData.getFloat("cmioValue")));
+            //User data
+            cmpisObject[i].setUserIBAN(cmpisData.getString("IBAN"));//userIBAN
+            cmpisObject[i].setUserNIF(cmpisData.getString("Nif"));//userNIF
+            cmpisObject[i].setUserAuthorized(cmpisData.getBoolean("Result"));//userAuthorized
+            cmpisObject[i].setUserValue(cmpisData.getFloat("Value"));//userValue
+            //Print CMIO Data
+            //System.out.println("CMIO IBAN: " + cmpisObject[i].getCmioIBAN());
+            //System.out.println("CMIO NIF: " + cmpisObject[i].getCmioNIF());
+            //System.out.println("CMIO Value: " + cmpisObject[i].getCmioValue());
+            //Print User Data
+            System.out.println("User IBAN: " + cmpisObject[i].getUserIBAN());
+            System.out.println("User NIF: " + cmpisObject[i].getUserNIF());
+            System.out.println("User Authorized: " + cmpisObject[i].getUserAuthorized());
+            System.out.println("User Value: " + cmpisObject[i].getUserValue()+"\n");
+        }
         //Disconnect
         con.disconnect();
-
     }
 
     private static void CMOIS() throws IOException {
@@ -93,7 +108,7 @@ public class TMAIS {
         con.setReadTimeout(5000);
         //Connection Status
         int status = con.getResponseCode();
-        System.out.println("CMOIS Response Code: "+ status);
+        System.out.println("CMOIS Response Code: " + status);
         //Read content
         BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
         String inputLine;
@@ -102,68 +117,41 @@ public class TMAIS {
             content.append(inputLine);
         }
         in.close();
-        //Create JSONObject
-        JSONObject cmoisData = new JSONObject(content.toString());
-        //Create Object
-        CMOISObject cmoisObject = new CMOISObject();
-        //DATA
-        cmoisObject.setCMSP(cmoisData.getString("CMSP"));
-        cmoisObject.setUserNIF(cmoisData.getString("userNIF"));
-        cmoisObject.setUserValue(cmoisData.getFloat("userValue"));
-        cmoisObject.setUserAuthorized(cmoisData.getBoolean("userAuthorized"));
-        //Print Data
-        System.out.println("CMSP IBAN: " + cmoisObject.getCMSP());
-
-        System.out.println("User NIF: " + cmoisObject.getUserNIF());
-        System.out.println("User Authorized: " + cmoisObject.getUserAuthorized());
-        System.out.println("User Value: " + cmoisObject.getUserValue());
+        //Create JSONArray
+        JSONArray jsonArray = new JSONArray(content.toString());
+        //Create array of objects
+        CMOISObject[] cmoisObject = new CMOISObject[jsonArray.length()];
+        //Create each object
+        for (int i = 0; i < jsonArray.length(); i++) {
+            //Create JSONObject
+            JSONObject cmoisData = jsonArray.getJSONObject(i);
+            //Create Object
+            cmoisObject[i] = new CMOISObject();
+            //DATA
+            cmoisObject[i].setCMSP(cmoisData.getString("IBAN"));
+            cmoisObject[i].setUserNIF(cmoisData.getString("Nif"));
+            cmoisObject[i].setUserAuthorized(cmoisData.getBoolean("Result"));
+            cmoisObject[i].setUserValue(cmoisData.getFloat("Value"));
+            //Print Data
+            System.out.println("CMSP IBAN: " + cmoisObject[i].getCMSP());
+            System.out.println("User NIF: " + cmoisObject[i].getUserNIF());
+            System.out.println("User Authorized: " + cmoisObject[i].getUserAuthorized());
+            System.out.println("User Value: " + cmoisObject[i].getUserValue()+"\n");
+        }
 
         //Disconnect
         con.disconnect();
     }
+}
+
+public class TMAIS {
+
+    public static void main(String[] args)
+    {
+        Schedule schedule = new Schedule();
+        Timer timer = new Timer();
+        timer.schedule(schedule,0, 20000);
+
+    }
 
 }
-/*
-        //Criar URL
-        URL url = new URL ("https://jsonplaceholder.typicode.com/todos/1");//"https://www.google.com");
-        //Ligar ao URL
-        HttpURLConnection con = (HttpURLConnection) url.openConnection();
-        //Definir Pedido
-        con.setRequestMethod("GET");
-
-        //Definir TimeOuts
-        con.setConnectTimeout(5000);
-        con.setReadTimeout(5000);
-
-        //Obter a Resposta
-        int status = con.getResponseCode();
-        System.out.println("Response Code: "+ status);
-
-        //Ler e Interpretar a Resposta
-        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-        String inputLine;
-        StringBuffer content = new StringBuffer();
-        while ((inputLine = in.readLine()) != null){
-            content.append(inputLine);
-        }
-        in.close();
-
-        JSONObject data = new JSONObject(content.toString());
-
-        System.out.println(content.toString());
-        System.out.println(data.toString());
-
-        CMPISObject falha = new CMPISObject();
-        falha.setUserId(Integer.parseInt(data.get("userId").toString()));
-        falha.setId(Integer.parseInt(data.get("id").toString()));
-        falha.setTitle(data.get("title").toString());
-        falha.setCompleted(data.getBoolean("completed"));
-
-        System.out.println("userId " + falha.getUserId());
-        System.out.println("id " + falha.getId());
-        System.out.println("title " + falha.getTitle());
-        System.out.println("completed " + falha.getCompleted());
-
-        //Desligar a Ligação
-        con.disconnect();
-*/
